1b4df83956693577790df3384a91e42608a933cf
Laura Caroselli <lcaroselli@outlook.com>
2017-08-14 17:21:27 -0600

    Build out iteration 0 method functionality

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,18 @@
 export default class DistrictRepository {
   constructor(data) {
-    this.data = {};
+    this.data = this.checkData(data)
+  }
+
+  checkData(rawData) {
+    return rawData.reduce((acc, object) => {
+
+      if(!acc[object.Location]) {
+        acc[object.Location] = [];
+      }
+      acc[object.Location].push(object);
+
+      return acc;
+
+    }, {})
   }
 }
