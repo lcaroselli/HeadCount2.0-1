f6fe42c41aee24a33533528d4a0eb94892f10461
Laura Caroselli <lcaroselli@outlook.com>
2017-08-17 14:58:59 -0600

    Test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('it exists', () => {
   const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
 });
