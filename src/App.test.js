4290a5163391954b8fe8e7c19aa93b76784bf24e
Laura Caroselli <lcaroselli@outlook.com>
2017-08-17 16:44:27 -0600

    Complete current app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import {shallow, mount} from 'enzym';
+import { shallow, mount } from 'enzyme';
 
-describe('App tests', () => {
+describe('App', () => {
   let wrapper;
 
   beforeEach( () => {
@@ -13,5 +13,23 @@ describe('App tests', () => {
   it('should exist', () => {
     expect(wrapper).toBeDefined();
   })
+
+  it('should render Background, Controls, CardDisplay components', () => {
+    expect(wrapper.find('Background').length).toEqual(1);
+    expect(wrapper.find('Controls').length).toEqual(1);
+    expect(wrapper.find('CardDisplay').length).toEqual(1);
+    })
+
+  it('should have an initial state of cards set to an array of matching data equal to 181 objects', () => {
+    expect(wrapper.state().cards.length).toEqual(181);
+  })
+
+  it ('should be able to display just the matching cards that we search for', () => {
+    wrapper = mount(<App />);
+    const searchInput = wrapper.find('.search-input');
+    searchInput.simulate('change', {target: { value: 'colorado'}});
+    expect(wrapper.state().cards.length).toEqual(2);
+  })
+
 
 })
