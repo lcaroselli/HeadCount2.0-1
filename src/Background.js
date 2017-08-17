d95535d808bf6d797a5581b2b037d9217ed5e7a4
Jason Lucas <jlucas5280@gmail.com>
2017-08-17 08:51:29 -0600

    start proptypes

--- src/Background.js
+++ src/Background.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import backgroundGraphic from '../assets/background.jpg'
+import backgroundGraphic from '../assets/background.jpg';
+import PropTypes from 'prop-types';
+import music from '../assets/01 Welcome to the Jungle.m4a';
 
 export default class Background extends Component {
   constructor() {
@@ -16,6 +18,10 @@ export default class Background extends Component {
         <section className='background-container'>
 
           <img className='background-graphic' src={backgroundGraphic}/>
+
+          <audio className='thunder-audio' autoPlay>
+            <source src={music} autoPlay/>
+          </audio>
         </section>
 
       </div>
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 class DistrictRepository {
   constructor(data) {
     this.data = this.checkData(data)
