d5c6231bb43313a8c8a30a78520072a44050f46a
Jason Lucas <jlucas5280@gmail.com>
2017-08-20 13:09:27 -0600

    comaprison function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import '../styles/Card.css';
 import '../styles/CardDisplay.css';
 import '../styles/Controls.css';
 import '../styles/background.css';
+import '../styles/compareDisplay.css';
 
 
 import App from './App';
