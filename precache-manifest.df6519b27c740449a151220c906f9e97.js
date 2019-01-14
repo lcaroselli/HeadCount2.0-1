d9a9db94ac5cc5eb58fed7ee9cf6850bc3f3d343
Laura Caroselli <lcaroselli@outlook.com>
2019-01-14 16:25:14 -0700

    Updates

---
+++ precache-manifest.df6519b27c740449a151220c906f9e97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fd7bcaff392f9c74cfea768018ddae4",
+    "url": "/HeadCount2.0-1/static/media/head-count-logo-white.8fd7bcaf.png"
+  },
+  {
+    "revision": "7e6c4fada6a8c5dd4069fa085ef634af",
+    "url": "/HeadCount2.0-1/static/media/background.7e6c4fad.jpg"
+  },
+  {
+    "revision": "c431fe336f788eb4d37d",
+    "url": "/HeadCount2.0-1/static/js/runtime~main.c431fe33.js"
+  },
+  {
+    "revision": "14c4272764cbdcb90408",
+    "url": "/HeadCount2.0-1/static/js/main.14c42727.chunk.js"
+  },
+  {
+    "revision": "28d9d8732135de7289da",
+    "url": "/HeadCount2.0-1/static/js/1.28d9d873.chunk.js"
+  },
+  {
+    "revision": "14c4272764cbdcb90408",
+    "url": "/HeadCount2.0-1/static/css/main.5a97d309.chunk.css"
+  },
+  {
+    "revision": "02ead4a4724bb199b916d817525a44a5",
+    "url": "/HeadCount2.0-1/index.html"
+  }
+];
